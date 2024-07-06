df24503955fde7bd504c483cd88f0368be42c8e5
ALERODAG <malaganet9@gmail.com>
2024-07-06 17:42:58 -0500

    Cargados con exito

---
+++ GA8-220501096-AA2-EV02/app/src/main/java/com/example/myapplication3/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.myapplication3;
+
+import static androidx.navigation.Navigation.findNavController;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import com.example.myapplication3.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+EditText textoNombre;
+TextView etiqueta;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textoNombre=findViewById(R.id.idTxt);
+        etiqueta=findViewById(R.id.textView5);
+
+
+
+
+//        BottomNavigationView navView = findViewById(R.id.nav_view);
+//        // Passing each menu ID as a set of Ids because each
+//        // menu should be considered as top level destinations.
+//        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+//                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+//                .build();
+//        NavController navController = findNavController(this, R.id.nav_host_fragment_activity_main);
+//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+//        NavigationUI.setupWithNavController(binding.navView, navController);
+    }
+
+
+
+    }
